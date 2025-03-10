b64a24e180ca470bf1ec1cb81a46b7b4edb8ca5e
Your Name <tinkamax03@gmail.com.com>
2025-03-10 21:39:38 +0300

    ex2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,52 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+    
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        
+        double gradePercentage;
+        if (!double.TryParse(input, out gradePercentage))
+        {
+            Console.WriteLine("Invalid input. Please enter a numeric value.");
+            return;
+        }
+
+        // Variable to hold the letter grade
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry! Keep trying, and you'll do better next time.");
+        }
     }
 }
